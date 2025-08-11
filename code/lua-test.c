2427390f0ab481da5375700c7a99371e986618e8
striter-no <ivansalasugin@gmail.com>
2025-08-11 20:44:30 +0300

    [upd] install and build scripts [upd] lua testing

--- code/lua-test.c
+++ code/lua-test.c
@@ -2,15 +2,22 @@
 #include <lua5.4/lualib.h>
 #include <lua5.4/lauxlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool ch_lua(lua_State *L, int result){
+    if (result != LUA_OK){
+        printf("Error: %s\n", lua_tostring(L, -1));
+        lua_pop(L, 1);
+        return false;
+    }
+    return true;
+}
 
 int main() {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    
-    if (luaL_dofile(L, "./assets/scripts/hello.lua") != LUA_OK){
-        printf("Error: %s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);
-    }
+
+    ch_lua(L, luaL_dofile(L, "./assets/scripts/hello.lua"));
 
     lua_close(L);
     return 0;

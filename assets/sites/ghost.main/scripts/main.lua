5b60492dfbc48fc1fbf98aa000b290596afd682a
striter-no <ivansalasugin@gmail.com>
2025-08-12 13:21:00 +0300

    [upd] fixed README and install scripts

--- assets/sites/ghost.main/scripts/main.lua
+++ assets/sites/ghost.main/scripts/main.lua
@@ -1,3 +1,5 @@
+
+
 -- Main script
 function OnInit()
     print("OnInit was called !2!");
@@ -6,4 +8,8 @@ end
 function OnTick(delta_time)
     -- print("current dt" .. delta_time)
     local a = delta_time + 1
+end
+
+function OnEvent(event)
+    ;
 end
